39e679057948363da351f9bb44ad383a050f8123
gdarquie <gaetan.darquie@gmail.com>
2021-04-18 12:58:26 +0200

    first commit

---
+++ Model/Fiction.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace Ficore\Model;
+
+
+class Fiction
+{
+    protected string $title;
+    protected string $author;
+    protected string $version = '0.1.0';
+
+    public function __construct(string $title, string $author = 'unknown')
+    {
+        $this->title = $title;
+        $this->author = $author;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param string $author
+     */
+    public function setAuthor(string $author): void
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion(): string
+    {
+        return $this->version;
+    }
+
+    /**
+     * @param string $version
+     */
+    public function setVersion(string $version): void
+    {
+        $this->version = $version;
+    }
+}
