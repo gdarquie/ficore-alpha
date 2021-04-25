0cac35e0246c5e449bc18de443ed2651b50e2777
gdarquie <gaetan.darquie@gmail.com>
2021-04-25 22:34:23 +0200

    Add scene

--- src/Model/Resource/Scene.php
+++ src/Model/Resource/Scene.php
@@ -4,7 +4,55 @@
 namespace Ficore\Model\Resource;
 
 
+use App\Entity\Story;
+use Ficore\Model\Action\Action;
+
 class Scene extends Resource
 {
+    private string $title;
+    private Story $story;
 
+    /**
+     * @var Action[]
+     */
+    private array $actions;
+
+    /**
+     * @return Story
+     */
+    public function getStory(): Story
+    {
+        return $this->story;
+    }
+
+    /**
+     * @param Story $story
+     */
+    public function setStory(Story $story): void
+    {
+        $this->story = $story;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return Action[]
+     */
+    public function getActions(): array
+    {
+        return $this->actions;
+    }
+
+    public function addActions(Action $action): array
+    {
+        $this->actions[] = $action;
+    }
 }
